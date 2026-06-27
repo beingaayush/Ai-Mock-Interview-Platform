3a249bca9b912519dc2313861ffab4e023864b3e
beingaayush <beingaayushraj@gmail.com>
2026-06-27 20:11:06 +0530

    updated routes

--- server/src/routes/interviewRoutes.js
+++ server/src/routes/interviewRoutes.js
@@ -1,19 +1,31 @@
+// const express = require("express");
+
+// const {
+//   startInterview,
+//   submitAnswer,
+//   generateInterviewReport,
+// } = require("../controllers/interviewController");
+// const { startSession, getNextQuestion } = require("../controllers/sessionController");
+
+
+// const router = express.Router();
+
+// router.post("/start-interview", startInterview);
+// router.post("/submit-answer", submitAnswer);
+// router.post("/generate-report", generateInterviewReport);
+
+// router.post("/start-session", startSession);
+// router.post("/next-question", getNextQuestion);
+
+// module.exports = router;
+
+
 const express = require("express");
-
-const {
-  startInterview,
-  submitAnswer,
-  generateInterviewReport,
-} = require("../controllers/interviewController");
 const { startSession, getNextQuestion } = require("../controllers/sessionController");
-
 
 const router = express.Router();
 
-router.post("/start-interview", startInterview);
-router.post("/submit-answer", submitAnswer);
-router.post("/generate-report", generateInterviewReport);
-
+// Sirf 2 simple routes
 router.post("/start-session", startSession);
 router.post("/next-question", getNextQuestion);
 
